fcc6ed613628c0543e9b87ce6cf1632336395ffa
Faraytodi Atmajaya <150215021+Faraysz@users.noreply.github.com>
2026-01-23 23:25:53 +0700

    Add TranslateController routes
    Added GET and POST routes for TranslateController. The root route now points to TranslateController@index, and a new /translate POST route was added for handling translations.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TranslateController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [TranslateController::class, 'index']);
+Route::post('/translate', [TranslateController::class, 'translate']);
